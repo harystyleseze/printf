5840d3a7ec2cbed8f1c44c92a1c08ef2a275a26a
codewithMahmouda <affamahmud7@gmail.com>
2023-10-17 20:42:10 -0700

    Add custom printf function

---
+++ _printf.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - mimic printf
+ * @format: parameter passed
+ *
+ * Return: number of character
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list argp;
+
+	va_start(argp, format);
+
+	if (format == NULL)
+		return (-1);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write_char(*format, &count);
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			if (*format == '%')
+			{
+				write_char('%', &count);
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(argp, int);
+
+				write_char(c, &count);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(argp, char*);
+
+				write_string(str, &count);
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(argp, int);
+
+				write_int(num, &count);
+			}
+		}
+		format++;
+	}
+	va_end(argp);
+	return (count);
+}
