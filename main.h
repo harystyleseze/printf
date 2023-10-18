8679255e6450b6f06a49feadc6f923e20294a236
codewithMahmouda <affamahmud7@gmail.com>
2023-10-17 22:18:40 -0700

    Hot fix!

--- main.h
+++ main.h
@@ -5,5 +5,6 @@ int _printf(const char *format, ...);
 void write_char(char c, int *count);
 void write_string(char *str, int *count);
 void write_int(int num, int *count);
+int _printf_func(const char *format, ...);
 
 #endif
